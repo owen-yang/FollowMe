bbf9ef42be1b53116c0445dd1a0df276027fd1d0
unknown <lstolz@eecs373-01.adsroot.itcs.umich.edu>
2015-04-14 14:15:33 -0400

    yolo

--- followme_car/SoftConsole/followme_car_MSS_MSS_CM3_0/followme_car_MSS_MSS_CM3_0_app/magnetometer.c
+++ followme_car/SoftConsole/followme_car_MSS_MSS_CM3_0/followme_car_MSS_MSS_CM3_0_app/magnetometer.c
@@ -1,5 +1,6 @@
 #include "magnetometer.h"
 
+magnetometer_data mag_data;
 
 void write8(const uint8_t reg, uint8_t value)
 {
@@ -19,7 +20,7 @@ void write8(const uint8_t reg, uint8_t value)
 void read_reg(const uint8_t reg, uint8_t *buf, size_t size)
 {
 	uint8_t target_addr = reg;
-	if (size > 1) target_addr |= 0x80;
+	// if (size > 1) target_addr |= 0x80; // sequential
 
 
 	MSS_I2C_write_read
@@ -33,30 +34,43 @@ void read_reg(const uint8_t reg, uint8_t *buf, size_t size)
 	);
 
 	MSS_I2C_wait_complete(&g_mss_i2c1, MSS_I2C_NO_TIMEOUT);
+}
 
-	/*
-	MSS_I2C_write
-		(
-			&g_mss_i2c1,
-			MAGNET_TARGET_ADDRESS,
-			&target_addr,
-			sizeof(target_addr),
-			MSS_I2C_HOLD_BUS
-		);
+void enable_magnetometer()
+{
+	write8(0x00, 0x10);
+	write8(0x01, 0x20);
+	write8(0x02, 0x00);
+}
 
-	MSS_I2C_wait_complete(&g_mss_i2c1, MSS_I2C_NO_TIMEOUT);
+void read_magnetometer()
+{
+	uint8_t desired_reg = 0x03;
+	uint8_t buf[] = {0, 0, 0, 0, 0, 0};
 
+	read_reg(desired_reg, buf, sizeof(buf));
 
-	MSS_I2C_read
+	mag_data.x = (int16_t)(buf[1] | ((int16_t)buf[0] << 8));
+	mag_data.y = (int16_t)(buf[5] | ((int16_t)buf[4] << 8));
+	mag_data.z = (int16_t)(buf[3] | ((int16_t)buf[2] << 8));
+}
+
+void send_over_XBee()
+{
+	uint8_t data[6] = {mag_data.x >> 8u, mag_data.x,
+					   mag_data.y >> 8u, mag_data.y,
+					   mag_data.z >> 8u, mag_data.z };
+	/* transmit the magnet data */
+	MSS_UART_polled_tx
 	(
-		&g_mss_i2c1,
-		MAGNET_TARGET_ADDRESS,
-		buf,
-		sizeof(buf),
-		MSS_I2C_RELEASE_BUS
+		&g_mss_uart1,
+		data, // data from magnetometer
+		sizeof(data)
 	);
+}
 
-	MSS_I2C_wait_complete(&g_mss_i2c1, MSS_I2C_NO_TIMEOUT);
-	*/
-	// return buf;
+void print_data()
+{
+	printf("X data: %x\n\rY data: %x\n\rZ data: %x\n\n\r",
+			mag_data.x, mag_data.y, mag_data.z);
 }
--- followme_car/SoftConsole/followme_car_MSS_MSS_CM3_0/followme_car_MSS_MSS_CM3_0_app/magnetometer.h
+++ followme_car/SoftConsole/followme_car_MSS_MSS_CM3_0/followme_car_MSS_MSS_CM3_0_app/magnetometer.h
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <inttypes.h>
+#include "drivers/mss_uart/mss_uart.h"
 #include "drivers/mss_i2c/mss_i2c.h"
 
 #define ACCEL_TARGET_ADDRESS 	(0x32 >> 1u)	// 0011001x
 #define MAGNET_TARGET_ADDRESS 	(0x3C >> 1u)	// 0011110x
 
+typedef struct magnetometer_data
+{
+	uint16_t x, y, z;
+} magnetometer_data;
+
+extern magnetometer_data mag_data;
+
 void write8(const uint8_t reg, uint8_t value);
 void read_reg(const uint8_t reg, uint8_t *buf, size_t size);
-
+void read_magnetometer();
+void send_over_XBee();
+void print_data();
